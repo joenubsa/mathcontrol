19c4fbb06b610fcc24888b2bc4e5d86d60fc507b
jorge.ubaque <jorge.ubaque@A19-DAP-305.aviatur.com.co>
2016-09-29 17:56:27 -0500

    por fin editor

--- web/app/vista/forma/administrar/modulo/articulo/articulo.js
+++ web/app/vista/forma/administrar/modulo/articulo/articulo.js
@@ -4,6 +4,9 @@ var Modulo = function () {
     var module = "administrar_articulo";
     var moduleFunctions = new ModuleFunctions(this);
     var moduleEvents = new ModuleEvents(this);
+    if (document.addEventListener) {
+        document.addEventListener('paste', moduleEvents.onPasteTriggered, false);
+    }
     var ListaArticulos = null;
     var articulosPicker = null;
     var removerEstilos = false;
@@ -28,15 +31,15 @@ var Modulo = function () {
             app.consultar(null, 'ListaArticulos', 'ListaArticulos');
         });
 
-        $('.text-editor-box .editionbox').on('keyup', function () {
-            actualizarEditorTextarea();
-        });
+//        $('.text-editor-box .editionbox').on('keyup', function () {
+//            actualizarEditorTextarea();
+//        });
         $('.text-editor-box .editionbox').on('change', function () {
             actualizarEditorTextarea();
         });
-        $('.text-editor-box .editionbox').on('input', function () {
-            actualizarEditorTextarea();
-        });
+//        $('.text-editor-box .editionbox').on('input', function () {
+//            actualizarEditorTextarea();
+//        });
         $('.text-editor-box .editionbox').on('propertyChanged', function () {
             actualizarEditorTextarea();
         });
@@ -89,6 +92,13 @@ var Modulo = function () {
             case "ListaArticulos":
                 cargarArticulos(r.content);
                 break;
+            case "result_":
+                switch (r.content.type) {
+                    case "cargarImagen":
+                        actualizarImagen(r.content.id, r.content.link)
+                        break;
+                }
+                break;
         }
     };
 
@@ -132,8 +142,36 @@ var Modulo = function () {
     };
 
     var actualizarEditorTextarea = function () {
-        $('#descripcion').val($('.text-editor-box .editionbox').html());
+        var contenidoActual = $('.text-editor-box .editionbox').html();
+        var nuevoContenido = procesarContenido(nuevoContenido);
+        $('#descripcion').val(contenidoActual);
     };
+
+    var procesarContenido = function (contenido) {
+        var editor = $('.text-editor-box .editionbox');
+        procesarImagenes(editor);
+    };
+
+    var procesarImagenes = function (editor) {
+        editor.find('img[src^=data]').each(function (identifier) {
+            var id = "img_" + identifier;
+            $(this).attr('id', id);
+            var addData = {
+                imageContent: JSON.stringify($(this).attr('src')),
+                id: id
+            };
+
+            app.ejecutar('cargarImagen', addData);
+        });
+    };
+    var actualizarImagen = function (id, filename) {
+        var ruta = "/public/images/articulo/" + filename;
+        $("#" + id).attr("src", ruta);
+//        var contenido = $('.text-editor-box .editionbox').html();
+//        contenido = contenido.replace($("#" + id)[0].outerHTML, '<div style="width:200px;height:200px;resize:both;">' + $("#" + id)[0].outerHTML + '</div>');
+//        $('.text-editor-box .editionbox').html(contenido);
+    };
+
 
     var removerEstilos = function () {
         var texto = $('.text-editor-box .editionbox').html();
@@ -209,6 +247,18 @@ var Modulo = function () {
             var toolBox = $('.text-editor-box .toolbox');
             toolBox.find('.icon').each(function () {
                 switch ($(this).attr('id')) {
+                    case "icon-h2":
+                        $(this).on('mousedown', events.h2Event);
+                        break;
+                    case "icon-h3":
+                        $(this).on('mousedown', events.h3Event);
+                        break;
+                    case "icon-i":
+                        $(this).on('mousedown', events.iEvent);
+                        break;
+                    case "icon-b":
+                        $(this).on('mousedown', events.bEvent);
+                        break;
                     case "icon-sup":
                         $(this).on('mousedown', events.supEvent);
                         break;
@@ -219,6 +269,27 @@ var Modulo = function () {
         };
 
         var eventCollection = function () {
+            this.h2Event = function (event) {
+                $(".text-editor-box .editionbox")[0].focus();
+                event.preventDefault();
+                insertTag('h2');
+            };
+            this.h3Event = function (event) {
+                $(".text-editor-box .editionbox")[0].focus();
+                event.preventDefault();
+                insertTag('h3');
+            };
+            this.iEvent = function (event) {
+                $(".text-editor-box .editionbox")[0].focus();
+                event.preventDefault();
+                insertTag('i');
+            };
+            this.bEvent = function (event) {
+                $(".text-editor-box .editionbox")[0].focus();
+                event.preventDefault();
+                insertTag('b');
+            };
+            
             this.supEvent = function (event) {
                 $(".text-editor-box .editionbox")[0].focus();
                 event.preventDefault();
@@ -229,22 +300,22 @@ var Modulo = function () {
                 selection = window.getSelection();
             };
         };
-        
-        var insertTag = function(tag){
-                //var newContent = '<' + tag + '>' + selection.toString() + '</' + tag + '>';
-                //console.log(newContent);
-                var node = document.createElement(tag);
-                var content = document.createTextNode(selection.toString());
-                node.appendChild(content);
-                var sel, range;
-                if (selection) {
-                    sel = selection;
-                    if (sel.rangeCount) {
-                        range = sel.getRangeAt(0);
-                        range.deleteContents();
-                        range.insertNode(node);
-                    }
+
+        var insertTag = function (tag) {
+            //var newContent = '<' + tag + '>' + selection.toString() + '</' + tag + '>';
+            //console.log(newContent);
+            var node = document.createElement(tag);
+            var content = document.createTextNode(selection.toString());
+            node.appendChild(content);
+            var sel, range;
+            if (selection) {
+                sel = selection;
+                if (sel.rangeCount) {
+                    range = sel.getRangeAt(0);
+                    range.deleteContents();
+                    range.insertNode(node);
                 }
+            }
         };
 
         var inicializar = function () {
@@ -260,5 +331,29 @@ var ModuleFunctions = function (modulo) {
 };
 
 var ModuleEvents = function (modulo) {
+    this.onPasteTriggered = function (e) {
+        if (typeof e.clipboardData !== 'undefined') {
+            var copiedData = e.clipboardData.items[0]; //Get the clipboard data
 
+            /*If the clipboard data is of type image, read the data*/
+            if (copiedData.type.indexOf("image") === 0) {
+                var imageFile = copiedData.getAsFile();
+
+                /*We will use HTML5 FileReader API to read the image file*/
+                var reader = new FileReader();
+
+                reader.onload = function (evt) {
+                    var result = evt.target.result; //base64 encoded image
+
+                    /*Create an image element and append it to the content editable div*/
+                    var img = document.createElement("img");
+                    img.src = result;
+                    $('.text-editor-box .editionbox').append(img);
+                };
+
+                /*Read the image file*/
+                reader.readAsDataURL(imageFile);
+            }
+        }
+    }
 };
